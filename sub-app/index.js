c3d8d85ff0673913e7fb84bae84ad823347a0a24
Jean-Charles Sisk <jasisk@gmail.com>
2015-08-20 16:08:15 -0400

    fixing sub-app to run standalone

--- sub-app/index.js
+++ sub-app/index.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var kraken = require('kraken-js');
 
+function onconfig(config, next) {
+  var conf = Object.create(config);
+  conf.get = function () {
+    return config.get.apply(config, arguments) ||
+      app.parent.kraken.get.apply(app.parent.kraken, arguments);
+  };
+  next(null, conf);
+}
+
 var app = express();
 var PORT = 8000;
 
-
 if (require.main === module) {
+  app.use(kraken());
   app.on('start', function () {
     app.listen(PORT).on('listening', function () {
       console.log('listening on %s ...', this.address().port || this.address());
@@ -15,16 +24,7 @@ if (require.main === module) {
   app.on('mount', function () {
     console.log('Sub-app mounted, initializing kraken ...');
     app.once('start', function () { console.log('sub-app kraken initialized!'); });
-    app.use(kraken({
-      onconfig: function (config, next) {
-        var conf = Object.create(config);
-        conf.get = function () {
-          return config.get.apply(config, arguments) ||
-            app.parent.kraken.get.apply(app.parent.kraken, arguments);
-        };
-        next(null, conf);
-      }
-    }));
+    app.use(kraken({ onconfig: onconfig }));
   });
 }
 
