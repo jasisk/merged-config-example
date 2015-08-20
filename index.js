48c898a67020a674e9a3b5d35fd047a73ab24446
Jean-Charles Sisk <jasisk@gmail.com>
2015-08-20 17:16:03 -0400

    waiting on child before listening

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 var express = require('express');
 var kraken = require('kraken-js');
 
+var check = thunk();
 var app = express();
 var PORT = 8000;
 
 app.use(kraken());
 
-app.on('start', function () {
+app.on('start', check.bind(null, 'start'));
+app.on('childStart', check.bind(null, 'childStart'));
+
+function thunk() {
+  var checks = ['start', 'childStart'];
+  return function (check) {
+    var idx;
+
+    console.log('Event: %s ...', check);
+    if (~(idx = checks.indexOf(check))) {
+      checks.splice(idx, 1);
+    }
+    if (!checks.length) {
+      oncomplete();
+    }
+  };
+}
+
+function oncomplete () {
   app.listen(PORT).on('listening', function () {
     console.log('listening on %s ...', this.address().port || this.address());
   });
-});
+}
--- sub-app/index.js
+++ sub-app/index.js
@@ -1,31 +1,45 @@
 var express = require('express');
 var kraken = require('kraken-js');
 
-function onconfig(config, next) {
-  var conf = Object.create(config);
-  conf.get = function () {
-    return config.get.apply(config, arguments) ||
-      app.parent.kraken.get.apply(app.parent.kraken, arguments);
-  };
-  next(null, conf);
-}
-
-var app = express();
 var PORT = 8000;
 
+function init(onstart) {
+  var app = express();
+
+  function onmount(onstart, opts) {
+    app.once('start', onstart);
+    app.use(kraken(opts));
+  }
+
+  function onconfig(config, next) {
+    var conf = Object.create(config);
+    conf.get = function () {
+      return config.get.apply(config, arguments) ||
+        app.parent.kraken.get.apply(app.parent.kraken, arguments);
+    };
+    next(null, conf);
+  }
+
+  onstart || (onstart = function () {app.parent.emit('childStart', app);});
+
+  if (require.main === module) {
+    onmount(onstart);
+  } else {
+    app.on('mount', function () {
+      console.log('Sub-app mounted ...');
+      onmount(onstart, {onconfig: onconfig});
+    });
+  }
+
+  return app;
+}
+
 if (require.main === module) {
-  app.use(kraken());
-  app.on('start', function () {
-    app.listen(PORT).on('listening', function () {
+  init(function () {
+    this.listen(PORT).on('listening', function () {
       console.log('listening on %s ...', this.address().port || this.address());
     });
   });
-} else {
-  app.on('mount', function () {
-    console.log('Sub-app mounted, initializing kraken ...');
-    app.once('start', function () { console.log('sub-app kraken initialized!'); });
-    app.use(kraken({ onconfig: onconfig }));
-  });
 }
 
-module.exports = function () { return app; };
+module.exports = init;
